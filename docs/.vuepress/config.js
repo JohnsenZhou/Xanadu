4920c50ff7483b7b0ef327f16e35f70803c0ef67
Johnsen <296240671@qq.com>
2020-02-24 18:59:48 +0800

    🛠 目录搭建

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -26,13 +26,75 @@ module.exports = {
         '',
       ],
       '/relearn/': [
-        '',
+        {
+          title: '重学前端',
+          collapsable: false,
+          children: [
+            ['', '介绍'],
+            {
+              title: 'JavaScript',
+              collapsable: true,
+              children: [
+                '/relearn/js/types'
+              ]
+            },
+            {
+              title: 'CSS',
+              collapsable: true,
+              children: [
+                '/relearn/css/types'
+              ]
+            },
+            {
+              title: 'HTML',
+              collapsable: true,
+              children: [
+                '/relearn/html/types'
+              ]
+            },
+            {
+              title: '浏览器实现原理与API',
+              collapsable: true,
+              children: [
+                '/relearn/browser/types'
+              ]
+            },
+            {
+              title: '前端综合应用',
+              collapsable: true,
+              children: [
+                '/relearn/allin/performance'
+              ]
+            },
+          ]
+        }
       ],
       '/algorithm/': [
         '',
       ],
       '/interview/': [
-        '',
+        {
+          title: '面试题',
+          collapsable: false,
+          children: [
+            ['', '介绍'],
+            {
+              title: 'JavaScript',
+              collapsable: true,
+              children: [
+                '/interview/js/debounce'
+              ]
+            },
+            {
+              title: 'CSS',
+              collapsable: true,
+              children: [
+                '/interview/css/bfc',
+                '/interview/css/center'
+              ]
+            }
+          ]
+        }
       ],
       '/react/': [
         '',
