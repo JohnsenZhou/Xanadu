ccbad8f7301dd847ca4dd900cb58de0d156114d1
JohnsenZhou <jiajunzhou1994@163.com>
2020-02-26 22:25:28 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,6 +24,7 @@ module.exports = {
       { text: '指南', link: '/guide/' },
       { text: '重学前端', link: '/relearn/' },
       { text: '算法', link: '/algorithm/' },
+      { text: '设计模式', link: '/pattern/' },
       { text: '面试题', link: '/interview/' },
       {
         text: '源码解析',
@@ -33,7 +34,115 @@ module.exports = {
         ]
       },
     ],
-    sidebar: require('./sidebar'),
+    sidebar: {
+      '/guide/': [
+        '',
+      ],
+      '/relearn/': [
+        {
+          title: '重学前端',
+          collapsable: false,
+          children: [
+            ['', '介绍'],
+            {
+              title: 'JavaScript',
+              collapsable: true,
+              children: [
+                '/relearn/js/types',
+                '/relearn/js/object'
+              ]
+            },
+            {
+              title: 'CSS',
+              collapsable: true,
+              children: [
+                // '/relearn/css/types'
+              ]
+            },
+            {
+              title: 'HTML',
+              collapsable: true,
+              children: [
+                // '/relearn/html/types'
+              ]
+            },
+            {
+              title: '浏览器实现原理与API',
+              collapsable: true,
+              children: [
+                '/relearn/browser/how'
+              ]
+            },
+            {
+              title: '前端综合应用',
+              collapsable: true,
+              children: [
+                '/relearn/allin/performance'
+              ]
+            },
+          ]
+        }
+      ],
+      '/algorithm/': [
+        {
+          title: '算法',
+          collapsable: false,
+          children: [
+            ['', '介绍'],
+            'complexity-analysis',
+            'array-linked-list',
+          ]
+        }
+      ],
+      '/interview/': [
+        {
+          title: '面试题',
+          collapsable: false,
+          children: [
+            ['', '介绍'],
+            {
+              title: 'JavaScript',
+              path: '/interview/js/',
+              collapsable: true,
+              children: [
+                '/interview/js/1',
+                '/interview/js/2',
+                '/interview/js/3',
+                '/interview/js/4',
+                '/interview/js/5',
+                '/interview/js/6'
+              ]
+            },
+            {
+              title: 'CSS',
+              path: '/interview/css/',
+              collapsable: true,
+              children: [
+                '/interview/css/1',
+                '/interview/css/2'
+              ]
+            },
+            {
+              title: '异步',
+              collapsable: true,
+              children: [
+                '/interview/async/1',
+                '/interview/async/2',
+              ]
+            }
+          ]
+        }
+      ],
+      '/pattern/': [
+        '',
+      ],
+      '/react/': [
+        '',
+      ],
+      '/vue/': [
+        '',
+      ],
+    },
     repo: 'JohnsenZhou/Xanadu',
     docsDir: 'docs',
     docsBranch: 'master',
--- docs/.vuepress/sidebar/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  '/guide/': require('./guide'),
-  '/relearn/': require('./relearn'),
-  '/algorithm/': require('./algorithm'),
-  '/interview/': require('./interview'),
-  '/react/': require('./react'),
-  '/vue/': require('./vue'),
-}
