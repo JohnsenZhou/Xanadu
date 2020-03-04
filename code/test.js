b7d043a1aaccc662855d67509d284eeae89dff3c
JohnsenZhou <jiajunzhou1994@163.com>
2020-03-04 23:11:49 +0800

    add chapter: bind模拟实现

--- code/test.js
+++ code/test.js
@@ -1,16 +1,19 @@
-Function.prototype.newCall = function (context, arr) {
-  context = context || window
-  context.fn = this
-  var result
-  if (!arr) {
-    return context.fn()
-  } else {
-    var args = []
-    for (var i = 1; i < arr.length; i++) {
-      args.push('arr[' + i + ']')
-    }
-    result = eval('context.fn(' + args + ')')
+Function.prototype.newBind = function (context) {
+  self = this
+  // 获取newBind函数从第二个参数到最后一个参数
+  var args = Array.prototype.slice.call(arguments, 1)
+  return function () {
+    // 这个时候的arguments是指bind返回的函数传入的参数
+    var bindArgs = Array.prototype.slice.call(arguments)
+    return self.apply(context, args.concat(bindArgs))
   }
-  delete context.fn
-  return result
 }
+var my = {
+  name: 'JohsnenZhou'
+}
+function say(age) {
+  console.log(this.name, age)
+}
+
+var newSay = say.newBind(my,26)
+newSay()
