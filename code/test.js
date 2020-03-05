324ee998f845320954ccc8a7140bf2ea62e84c6a
Johnsen <296240671@qq.com>
2020-03-05 09:57:43 +0800

    add article: bind的实现

--- code/test.js
+++ code/test.js
@@ -1,19 +1,17 @@
-Function.prototype.newBind = function (context) {
-  self = this
-  // 获取newBind函数从第二个参数到最后一个参数
-  var args = Array.prototype.slice.call(arguments, 1)
-  return function () {
-    // 这个时候的arguments是指bind返回的函数传入的参数
-    var bindArgs = Array.prototype.slice.call(arguments)
-    return self.apply(context, args.concat(bindArgs))
-  }
-}
 var my = {
   name: 'JohsnenZhou'
 }
-function say(age) {
-  console.log(this.name, age)
+function say(age, sex) {
+  this.habit = 'shopping'
+  console.log(this.name)
+  console.log(age)
+  console.log(sex)
 }
-
-var newSay = say.newBind(my,26)
-newSay()
+say.prototype.friend = 'anna.'
+var newSay = say.bind(my, 18)
+var obj = new newSay('男')
+// undefined
+// 18
+// 男
+console.log(obj.friend) // anna.
+console.log(obj.habit) // shopping
