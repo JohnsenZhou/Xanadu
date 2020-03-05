627c2d8ae224208d837f575de98d33d070f05192
JohnsenZhou <jiajunzhou1994@163.com>
2020-03-05 23:37:37 +0800

    theme

--- packages/vuepress-theme-xanadu/layouts/Layout.vue
+++ packages/vuepress-theme-xanadu/layouts/Layout.vue
@@ -1,17 +1,19 @@
 <template>
   <ParentLayout>
-    <!-- <template #sidebar-top>
-      <CarbonAds />
-    </template> -->
+    <template #sidebar-top>
+      <Ads/>
+    </template>
   </ParentLayout>
 </template>
 
 <script>
 import ParentLayout from '@parent-theme/layouts/Layout.vue'
+import Ads from '@theme/components/Ads.vue'
 export default {
   name: 'Layout',
   components: {
-    ParentLayout
+    ParentLayout,
+    Ads
   }
 }
 </script>
