6971b613d5aba4f06c23182c11342a6d2df59880
Johnsen <296240671@qq.com>
2020-03-11 16:28:34 +0800

    add article: 发布-订阅模式

--- code/test.js
+++ code/test.js
@@ -1,46 +1,22 @@
-class Events {
-  constructor() {
-    this.cache = {}
+const loginEvent = new Events();
+const header = {
+  setAvatar: function() {
+    console.log("设置header模块的头像");
   }
-  listen(key, fn) {
-    if (!this.cache[key]) {
-      this.cache[key] = []
-    }
-    this.cache[key].push(fn)
+};
+const nav = {
+  setAvatar: function() {
+    console.log("设置导航模块的头像");
   }
-  trigger() {
-    const key = Array.prototype.shift.call(arguments)
-    const fns = this.cache[key]
-    if (!fns || fns.length === 0) return
-    fns.forEach(fn => {
-      fn.apply(this, arguments)
-    })
+};
+const message = {
+  refresh: function() {
+    console.log("刷新消息列表");
   }
-  remove(key, fn) {
-    const fns = this.cache[key]
-    if (!fns) return
-    if (fn) {
-      fns.forEach((item, index) => {
-        if (item === fn) {
-          fns.splice(index, 1)
-        }
-      })
-    } else {
-      fns.length = 0
-      console.log(this.cache)
-    }
-  }
-}
-
-const salesOffices = new Events()
-
-salesOffices.listen('88m2',fn1 = function(price) {
-  console.log('88m2现在价格是111：', price)
-})
-salesOffices.listen('88m2',fn2 = function(price) {
-  console.log('88m2现在价格是2222：', price)
-})
-
-salesOffices.listen('100m2', function(price) {
-  console.log('100m2现在价格是：', price)
-})
+};
+loginEvent.listen("loginSuccess", header.setAvatar);
+loginEvent.listen("loginSuccess", nav.setAvatar);
+loginEvent.listen("loginSuccess", message.refresh);
+$.ajax("http://xxx.com/login", data => {
+  loginEvent.trigger("loginSuccess", data); // 发布登陆成功消息
+});
