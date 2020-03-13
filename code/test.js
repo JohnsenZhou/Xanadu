ce1bbf4c550116e73c171b1c56dfc4ba045b1c57
Johnsen <296240671@qq.com>
2020-03-13 17:15:58 +0800

    add article: 组合模式

--- code/test.js
+++ code/test.js
@@ -1,22 +1,57 @@
-const loginEvent = new Events();
-const header = {
-  setAvatar: function() {
-    console.log("设置header模块的头像");
+class OrderBox {
+  constructor(name) {
+    this.name = name;
+    this.products = [];
   }
-};
-const nav = {
-  setAvatar: function() {
-    console.log("设置导航模块的头像");
+  add(product) {
+    this.products.push(product);
   }
-};
-const message = {
-  refresh: function() {
-    console.log("刷新消息列表");
+  sum() {
+    let result = 0;
+    this.products.forEach(item => {
+      result = result + item.sum();
+    });
+    console.log(`开始计算${this.name}下总金额，结果为${result}`);
+    return result;
   }
-};
-loginEvent.listen("loginSuccess", header.setAvatar);
-loginEvent.listen("loginSuccess", nav.setAvatar);
-loginEvent.listen("loginSuccess", message.refresh);
-$.ajax("http://xxx.com/login", data => {
-  loginEvent.trigger("loginSuccess", data); // 发布登陆成功消息
-});
+}
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+  add() {
+    throw new Error("已是产品类型，没法继续添加产品");
+  }
+  sum() {
+    return this.price;
+  }
+}
+
+const order = new OrderBox("总订单");
+const order1 = new OrderBox("订单1");
+const order2 = new OrderBox("订单2");
+const order3 = new OrderBox("订单3");
+
+const product1 = new Product("产品1", 2);
+const product2 = new Product("产品2", 33);
+const product3 = new Product("产品3", 44);
+const product4 = new Product("产品4", 55);
+const product5 = new Product("产品5", 66);
+
+order1.add(product1);
+order1.add(product2);
+order3.add(product3);
+order2.add(product4);
+order2.add(order3);
+
+order.add(order1);
+order.add(order2);
+order.add(product5);
+
+order.sum();
+
+// 开始计算订单1下总金额，结果为35
+// 开始计算订单3下总金额，结果为44
+// 开始计算订单2下总金额，结果为99
+// 开始计算总订单下总金额，结果为200
---
+++ docs/.vuepress/components/ImageDesc.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <img class="image" :style="{ 'max-width': imgWidth }" :src="src" />
+    <div class="desc" v-if="desc">{{ desc }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    desc: {
+      type: String,
+      default: ""
+    },
+    imgWidth: {
+      type: String,
+      default: "50%"
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.container
+  width 100%
+  text-align center
+  .image {
+  }
+  .desc {
+    display inline-block
+    width 80%
+    margin-top 14px
+    font-size 14px
+    color #999
+  }
+</style>
