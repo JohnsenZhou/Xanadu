bc5e8f14bdc218d71fa8f9d8c5f45ccb5e9e1229
Johnsen <296240671@qq.com>
2020-03-25 17:15:27 +0800

    add article: 职责链模式

--- code/test.js
+++ code/test.js
@@ -1,62 +1,59 @@
-class Upload {
-  constructor(type) {
-    this.uploadType = type;
-  }
-  delFile() {
-    this.dom.parentNode.removeChild(this.dom);
-  }
-}
-
-const uploadFactory = (function() {
-  const createdUploadObj = {};
-  return {
-    create(uploadType) {
-      if (createdUploadObj[uploadType]) {
-        return createdUploadObj[uploadType];
-      }
-      return (createdUploadObj[uploadType] = new Upload(uploadType));
-    }
-  };
-})();
-
-const uploadManager = (function() {
-  const uploadDatabase = {};
-  return {
-    add: (id, uploadType, fileName) => {
-      const flyweightObj = UploadFactory.create(uploadType);
-      const dom = document.createElement("div");
-      dom.innerHTML =
-        "＜span>文件名称：" +
-        fileName +
-        "</span>" +
-        '＜button class="delFile">删除＜／button>';
-      dom.querySelector(".delFile").onclick = function() {
-        flyweightObj.delFile(id);
-      };
-      document.body.appendChild(dom);
-      uploadDatabase[id] = {
-        fileName,
-        dom
-      };
-      return flyweightObj;
-    }
-  };
-})();
-
-window.startUpload = (uploadType, files) => {
-  for (let i = 0; i < files.length; i++) {
-    const uploadObj = uploadManager.add(++id, uploadType, file.fileName);
+const ORDER_TYPE_RESERVED_500 = 1; // 预付500元定金用户
+const ORDER_TYPE_RESERVED_200 = 2; // 预付200元定金用户
+const ORDER_TYPE_NORMAL = 3; // 普通购买用户
+const order500 = (orderType, payed, stock) => {
+  if (payed && orderType === ORDER_TYPE_RESERVED_500) {
+    console.log("500元定金预购，得到100优惠券");
+  } else {
+    // 未支付定金
+    return "nextSuccessor";
   }
 };
 
-startUpload("iframe", [
-  { fileName: "1.txt" },
-  { fileName: "2.txt" },
-  { fileName: "3.txt" }
-]);
+const order200 = (orderType, payed, stock) => {
+  if (payed && orderType === ORDER_TYPE_RESERVED_200) {
+    console.log("200元定金预购，得到50优惠券");
+  } else {
+    // 未支付定金
+    return "nextSuccessor";
+  }
+};
 
-startUpload("form", [
-  { fileName: "4.txt" },
-  { fileName: "5.txt" },
-  { fileName: "6.txt" }
-]);
+const orderNormal = (orderType, payed, stock) => {
+  if (stock > 0) {
+    console.log("普通购买，无优惠券");
+  } else {
+    console.log("手机库存不足");
+  }
+};
+
+class Chain {
+  constructor(fn) {
+    this.fn = fn;
+    this.nextSuccessor = null;
+  }
+  setNextSuccessor(successor) {
+    this.nextSuccessor = successor;
+  }
+  passRequest() {
+    const result = this.fn.apply(this, arguments);
+    if (result === "nextSuccessor") {
+      return (
+        this.nextSuccessor &&
+        this.nextSuccessor.passRequest.apply(this.nextSuccessor, arguments)
+      );
+    }
+    return result;
+  }
+}
+const chainOrder500 = new Chain(order500);
+const chainOrder200 = new Chain(order200);
+const chainOrderNormal = new Chain(orderNormal);
+
+chainOrder500.setNextSuccessor(chainOrder200);
+chainOrder200.setNextSuccessor(chainOrderNormal);
+
+chainOrder500.passRequest(1, true, 500);
+chainOrder500.passRequest(2, true, 500);
+chainOrder500.passRequest(3, true, 500);
+chainOrder500.passRequest(1, false, 0);
