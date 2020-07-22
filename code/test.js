ddba3ee060d28e72a9f6ff791acbc372648255b6
Johnsen <296240671@qq.com>
2020-07-22 20:05:54 +0800

    update

--- code/test.js
+++ code/test.js
@@ -1,14 +1,31 @@
-function twoSum(nums = [], target) {
-  let result;
-  for (let i = 0; i < nums.length; i++) {
-    if (result) break;
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        result = [i, j];
-        break;
-      }
+function generate(arr) {
+  let result = [];
+  let i = 1;
+  while (arr.length) {
+    if (i % 2) {
+      result.push(arr.shift());
+    } else {
+      arr.push(arr.shift());
     }
+    i++;
   }
   return result;
 }
-console.log(twoSum([1, 2, 3, 4, 5], 5));
+
+function reverse(arr) {
+  let i = 1;
+  let out = [];
+  while (arr.length) {
+    if (i % 2) {
+      out.unshift(arr.pop());
+    } else {
+      out.unshift(out.pop());
+    }
+    i++;
+  }
+  return out;
+}
+
+const dd = reverse([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]);
+const ff = generate([1, 12, 2, 8, 3, 11, 4, 9, 5, 13, 6, 10, 7]);
+console.log(dd, ff);
