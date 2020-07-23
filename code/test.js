492a8cd2b516cfef05a05868d4ecdf551562368c
Johnsen <296240671@qq.com>
2020-07-23 19:18:21 +0800

    update

--- code/test.js
+++ code/test.js
@@ -1,31 +1,5 @@
-function generate(arr) {
-  let result = [];
-  let i = 1;
-  while (arr.length) {
-    if (i % 2) {
-      result.push(arr.shift());
-    } else {
-      arr.push(arr.shift());
-    }
-    i++;
-  }
-  return result;
+function sum(num) {
+  if (num <= 2) return num;
+  return sum(num - 1) + sum(num - 2);
 }
-
-function reverse(arr) {
-  let i = 1;
-  let out = [];
-  while (arr.length) {
-    if (i % 2) {
-      out.unshift(arr.pop());
-    } else {
-      out.unshift(out.pop());
-    }
-    i++;
-  }
-  return out;
-}
-
-const dd = reverse([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]);
-const ff = generate([1, 12, 2, 8, 3, 11, 4, 9, 5, 13, 6, 10, 7]);
-console.log(dd, ff);
+console.log(sum(5));
