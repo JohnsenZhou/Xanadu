fbec652d161f823916831295aafed465ba1729a9
JohnsenZhou <jiajunzhou1994@163.com>
2020-07-28 22:43:27 +0800

    update

--- code/test.js
+++ code/test.js
@@ -1,19 +1,4 @@
-function find(arr = [], num) {
-  const len = arr.length
-  let map = {}
-  let temp = 0
-  for (let i = 0; i < len; i++) {
-    temp = 0
-    for (let j = i; j < len; j++) {
-      temp += arr[j]
-      if (temp === num) {
-        map[j - i] = [i, j]
-      }
-    }
-  }
-  if (Object.keys(map).length) {
-    const key = Math.min(...Object.keys(map))
-    return arr.slice(map[key][0], map[key][1] + 1)
-  }
-  return 0
+function restoreIpAddresses(str = '') {
+  const result = []
+  if (str.length < 4 || str.length > 12) return []
 }
